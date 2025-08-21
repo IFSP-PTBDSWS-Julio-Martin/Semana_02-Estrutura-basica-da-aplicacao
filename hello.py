c65d5f67d343b708c18e0ded5be57496bffd3b6e
JulioPMartin <julio.martin@aluno.ifsp.edu.br>
2025-08-21 19:32:07 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,20 +6,42 @@ from flask import abort
 
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    # return '<p>Alterações por meio do GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
-    return '<h1>Hello World!</h1> <h2>Disciplina PTBDSWS</h2>'
+def home():
+    return '''
+    <h1>Avaliação contínua: Aula 030</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/user/Julio%20Pereira%20Martin/PT3031608/IFSP">Identificação</a></li>
+        <li><a href="/contextorequisicao">Contexto da requisição</a></li>
+    </ul>
+    '''
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario, instituicao):
+    return f'''
+    <h1>Avaliação contínua: Aula 030</h1>
+    <h2>Aluno: {name}</h2>
+    <h2>Prontuário: {prontuario}</h2>
+    <h2>Instituição: {instituicao}</h2>
+    <br>
+    <a href="/">Voltar</a>
+    '''
 
 @app.route('/contextorequisicao')
 def req():
-    cont_req = request.headers.get('User-Agent') # informação que o servidor pega para saber do navegador e so
-    return cont_req
+    cont_req = request.headers.get('User-Agent')
+    ip_usuario = request.remote_addr
+    host = request.host
+    return f'''
+    <h1>Avaliação contínua: Aula 030</h1>
+    <h2>Seu navegador é: {cont_req}</h2>
+    <h2>O IP do cumputador remoto é: {ip_usuario}</h2>
+    <h2>O host da aplicação é: {host}</h2>
+    <br>
+    <a href="/">Voltar</a>
+    '''
 
-@app.route('/codigostatusdiferente')
+''' @app.route('/codigostatusdiferente')
 def status():
     return '<p>Bad request</p>'
 
@@ -34,3 +56,4 @@ def redirecionar():
 @app.route('/abortar')
 def abortar():
     return abort(404)
+'''
